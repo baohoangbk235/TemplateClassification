bc6b0c1ee8b410e243b047a680ec4e984b47aba1
baohoangbk235 <baohoangbk235@gmail.com>
2021-03-26 15:01:42 +0700

    add triplet dataset

--- utils/dataset.py
+++ utils/dataset.py
@@ -5,9 +5,8 @@ import argparse
 import os
 import torch
 import numpy as np 
-
 import cv2
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 
@@ -34,21 +33,46 @@ class TemplateDataset(Dataset):
     def __len__(self):
         return len(self.list_images)
     
+    def get_triplet(self, anchor_label, neg_label):
+        image_filenames = os.listdir(os.path.join(self.root_dir, anchor_label))
+        anchor_filename = random.choice(image_filenames)
+        anchor_path = os.path.join(self.root_dir, anchor_label, anchor_filename)
+        anchor_img = cv2.imread(anchor_path.replace(os.sep, '/'))
+
+        pos_filename = random.choice([pos for pos in image_filenames if pos != anchor_filename])
+        pos_path = os.path.join(self.root_dir, anchor_label, pos_filename)
+        pos_img = cv2.imread(pos_path.replace(os.sep, '/'))
+
+        neg_filename = random.choice(os.listdir(os.path.join(self.root_dir, neg_label)))
+        neg_path = os.path.join(self.root_dir, neg_label, neg_filename)
+        neg_img = cv2.imread(neg_path.replace(os.sep, '/'))
+
+        return anchor_img, pos_img, neg_img
+    
+    def show_triplet(self, images):
+        titles = ["Anchor", "Positive", "Negative"]
+        plt.figure(figsize=(12,4))
+        for i in range(3):
+            plt.subplot(1, 3, i+1)
+            plt.imshow(images[i].astype("uint8"))
+            plt.title(titles[i])
+        plt.show()
+    
     def __getitem__(self, index: int):
         label = self.labels[index]
-        self.image_filenames = os.listdir(os.path.join(self.root_dir, label))
-        filename = random.choice(self.image_filenames)
-        img_path = os.path.join(self.root_dir, self.label, filename)
-        print(img_path)
-        # img = cv2.imread(img_path)
-        # show_image(img)
-        # if self.transforms:
-        #     img = self.transforms({"image": img})["image"]
-        return img
+        neg_label = random.choice([neg_ for neg_ in self.labels if neg_ != label])
+
+        anchor, pos, neg = self.get_triplet(label, neg_label)
+        self.show_triplet([anchor, pos, neg])
+        if self.transforms:
+            anchor = self.transforms({"image": anchor})["image"]
+            pos = self.transforms({"image": pos})["image"]
+            neg = self.transforms({"image": neg})["image"]
+        return [anchor, pos, neg], [label, label, neg_label]
 
 if __name__ == "__main__":
     if args.path is None:
         dataset = TemplateDataset("BK_table_data/table")  
     else:
         dataset = TemplateDataset(args.path)
-    sample = dataset[1]
+    sample = dataset[2]
